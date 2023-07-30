32307250d34e1abafec3ade7ce9ffc2c75b9cbd3
beomsun1234 <beomsun22@gmail.com>
2023-07-30 20:37:26 +0900

    refactor: #1 중복 코드 제거 및 token 요청 시 MultiValueMap으로 변환하는 로직 dto에 추가

--- src/main/java/com/bs/openbanking/bank/api/OpenBankApiClient.java
+++ src/main/java/com/bs/openbanking/bank/api/OpenBankApiClient.java
@@ -1,21 +1,13 @@
 package com.bs.openbanking.bank.api;
 
 import com.bs.openbanking.bank.dto.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,9 +16,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 @Service
 public class OpenBankApiClient {
-    private final OpenBankutil openBankutil;
+    private final OpenBankUtil openBankutil;
     private final HttpHeaders httpHeaders;
     private final RestTemplate restTemplate;
+
     @Value("${openbank.useCode}")
     private String useCode; // 핀테크번호+U -> 거래고유번호 생성기
     @Value("${openbank.client-id}")
@@ -34,71 +27,64 @@ public class OpenBankApiClient {
     @Value("${openbank.client-secret}")
     private String client_secret;
 
-    private String redirect_uri = "http://localhost:8080/auth/openbank/callback";
-    private String base_url = "https://testapi.openbanking.or.kr/v2.0";
+    private final static String redirect_uri = "http://localhost:8080/auth/openbank/callback";
+    private final static String base_url = "https://testapi.openbanking.or.kr/v2.0";
 
     /**
      * 토큰발급요청
+     * post 방식으로 key=vale 데이터 요청 (금결원)
+     * 토큰요청 http 헤더타입은 application/x-www-form-urlencoded
      */
     public BankReponseToken requestToken(BankRequestToken bankRequestToken){
-        //post 방식으로 key=vale 데이터 요청 (금결원)
-        //http 헤더 오브젝트 생성
         httpHeaders.add("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
-        //httpBody 오브젝트 생성
+
         bankRequestToken.setBankRequestToken(clientId,client_secret,redirect_uri,"authorization_code");
-        //헤더의 컨텐트 타입이 application/x-www-form-urlencoded;charset=UTF-8이므로 객체를 집어넣을수 없음.. 그러므로 MultiValueMap 사용 해야함
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("code",bankRequestToken.getCode());
-        parameters.add("client_id",bankRequestToken.getClient_id());
-        parameters.add("client_secret",bankRequestToken.getClient_secret());
-        parameters.add("redirect_uri",bankRequestToken.getRedirect_uri());
-        parameters.add("grant_type",bankRequestToken.getGrant_type());
-        // HttpHeader 와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String,String>> param =
-                new HttpEntity<>(parameters,httpHeaders);
-        //Http 요청하기 - post 방식으로
-        return restTemplate.exchange("https://testapi.openbanking.or.kr/oauth/2.0/token", HttpMethod.POST, param, BankReponseToken.class).getBody();
+        /**
+         * 헤더의 컨텐트 타입이 application/x-www-form-urlencoded;charset=UTF-8이므로 객체를 집어넣을수 없음.. 그러므로 MultiValueMap 사용 해야함
+         */
+        HttpEntity httpEntity = generateHttpEntityWithBody(httpHeaders, bankRequestToken.toMultiValueMap());
+
+        return restTemplate.exchange("https://testapi.openbanking.or.kr/oauth/2.0/token",HttpMethod.POST, httpEntity ,BankReponseToken.class).getBody();
     }
-    //        //Gson, Json Simple, Object Mapper
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        BankReponseToken bankReponseToken = null;
-//        try {
-//            bankReponseToken = objectMapper.readValue(responseEntity.getBody(), BankReponseToken.class);
-//
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
+    private HttpEntity generateHttpEntityWithBody(HttpHeaders httpHeaders, MultiValueMap body) {
+        return new HttpEntity<>(body, httpHeaders);
+    }
 
     /**
-     * 계좌조히
+     * 계좌조회
      * @param accountSearchRequestDto
      * @return
      */
-    public BankAcountSearchResponseDto requestAccountList(AccountSearchRequestDto accountSearchRequestDto){
+    public BankAccountSearchResponseDto requestAccountList(AccountSearchRequestDto accountSearchRequestDto){
         String url = base_url+"/account/list";
-        HttpEntity<String> openBankAcountSerchRequest = new HttpEntity<>(setHeader(accountSearchRequestDto.getAccess_token()));
+
+        HttpEntity httpEntity = generateHttpEntity(generateAuthHeader(accountSearchRequestDto.getAccess_token()));
+
         UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
                 .queryParam("user_seq_no", accountSearchRequestDto.getUser_seq_no())
                 .queryParam("include_cancel_yn", accountSearchRequestDto.getInclude_cancel_yn())
                 .queryParam("sort_order", accountSearchRequestDto.getSort_order())
                 .build();
 
-        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, openBankAcountSerchRequest, BankAcountSearchResponseDto.class).getBody();
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET,httpEntity , BankAccountSearchResponseDto.class).getBody();
     }
     /**
      * 잔액조회
      */
     public BankBalanceResponseDto requestBalance(BankBalanceRequestDto bankBalanceRequestDto, String access_token){
         String url = base_url+"/account/balance/fin_num";
-        HttpEntity<String> balance = new HttpEntity<>(setHeader(access_token));
+
+        HttpEntity httpEntity = generateHttpEntity(generateAuthHeader(access_token));
+
         bankBalanceRequestDto.setBankTransIdAndTranssDtime(openBankutil.getRandomNumber(bankBalanceRequestDto.getBank_tran_id()),openBankutil.getTransTime());
+
         UriComponents builder = UriComponentsBuilder.fromHttpUrl(url)
                 .queryParam("bank_tran_id",bankBalanceRequestDto.getBank_tran_id())
                 .queryParam("fintech_use_num", bankBalanceRequestDto.getFintech_use_num())
                 .queryParam("tran_dtime",bankBalanceRequestDto.getTran_dtime())
                 .build();
 
-        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, balance, BankBalanceResponseDto.class).getBody();
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, BankBalanceResponseDto.class).getBody();
     }
 
     /**
@@ -107,18 +93,26 @@ public class OpenBankApiClient {
      */
     public AccountTransferResponseDto requestTransfer(String access_token, AccountTransferRequestDto accountTransferRequestDto){
         String url = base_url+"//transfer/withdraw/fin_num";
+
         accountTransferRequestDto.setTran_dtime(openBankutil.getTransTime());
-        ResponseEntity<AccountTransferRequestDto> param = new ResponseEntity<>(accountTransferRequestDto,setHeader(access_token),HttpStatus.OK);
+
+        ResponseEntity<AccountTransferRequestDto> param = new ResponseEntity<>(accountTransferRequestDto,generateAuthHeader(access_token),HttpStatus.OK);
+
         return restTemplate.exchange(url, HttpMethod.POST, param, AccountTransferResponseDto.class).getBody();
     }
 
+    /**
+     * 요청할 HttpEntity 생성
+    */
+    private HttpEntity generateHttpEntity(HttpHeaders httpHeaders) {
+
+        return new HttpEntity<>(httpHeaders);
+    }
 
     /**
      * 헤더에 엑세스 토큰넣기
-     * @param access_token
-     * @return
      */
-    public HttpHeaders setHeader(String access_token){
+    private HttpHeaders generateAuthHeader(String access_token){
         httpHeaders.add("Authorization", "Bearer "+access_token);
         return httpHeaders;
     }
--- src/main/java/com/bs/openbanking/bank/dto/BankRequestToken.java
+++ src/main/java/com/bs/openbanking/bank/dto/BankRequestToken.java
@@ -2,6 +2,8 @@ package com.bs.openbanking.bank.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Data
 public class BankRequestToken {
@@ -17,5 +19,16 @@ public class BankRequestToken {
         this.client_secret = client_secret;
         this.redirect_uri = redirect_uri;
         this.grant_type = grant_type;
+    }
+
+    public MultiValueMap<String, String> toMultiValueMap() {
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("code",this.code);
+        parameters.add("client_id",this.client_id);
+        parameters.add("client_secret",this.client_secret);
+        parameters.add("redirect_uri", this.redirect_uri);
+        parameters.add("grant_type",this.grant_type);
+
+        return parameters;
     }
 }
