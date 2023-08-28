96275594fdaa0b29d2b7f3ed3a422a813ec11fba
beomsun1234 <beomsun22@gmail.com>
2023-08-28 18:30:56 +0900

    Feat: #1 회원 조회 exist문 추가

--- src/main/java/com/bs/openbanking/bank/repository/MemberRepository.java
+++ src/main/java/com/bs/openbanking/bank/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.bs.openbanking.bank.repository;
 
 import com.bs.openbanking.bank.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
+    boolean existsMemberById(Long id);
 }
