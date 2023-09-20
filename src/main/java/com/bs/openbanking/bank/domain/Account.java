17bd9ff9767eb2fa49e06d2499f8b42cd7cdc456
beomsun1234 <beomsun22@gmail.com>
2023-09-20 19:37:35 +0900

    feat: 주계좌 설정

--- src/main/java/com/bs/openbanking/bank/domain/Account.java
+++ src/main/java/com/bs/openbanking/bank/domain/Account.java
@@ -3,17 +3,19 @@ package com.bs.openbanking.bank.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@NoArgsConstructor
 @Table(
         indexes = {
                 @Index(name = "idx_member_id", columnList = "member_id", unique = false)
         }
 )
+@NoArgsConstructor
 public class Account extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +28,26 @@ public class Account extends BaseTimeEntity{
     private String accountNum;
     private String bankCode;
     private String accountSeq;
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
+    private String holderName;
+
+    @PrePersist
+    public void prePersist() {
+        this.accountType = this.accountType == null ? AccountType.SUB : this.accountType;
+    }
+
+    public boolean isMainAccount(){
+        if (this.accountType == AccountType.MAIN) return true;
+        return false;
+    }
+
+    public void updateAccountType(AccountType accountType){
+        this.accountType = accountType;
+    }
 
     @Builder
-    public Account(Long id, Long memberId, String fintechUseNum, String bankName, String accountNum, String bankCode, String accountSeq) {
+    public Account(Long id, Long memberId, String fintechUseNum, String bankName, String accountNum, String bankCode, String accountSeq, String holderName, AccountType accountType) {
         this.id = id;
         this.memberId = memberId;
         this.fintechUseNum = fintechUseNum;
@@ -36,5 +55,7 @@ public class Account extends BaseTimeEntity{
         this.accountNum = accountNum;
         this.bankCode = bankCode;
         this.accountSeq = accountSeq;
+        this.holderName = holderName;
+        this.accountType = accountType;
     }
 }
